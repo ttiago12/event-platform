4b82c15351896cd96d2d35c2e5c97b2f58b11424
ttiago12 <74989635+ttiago12@users.noreply.github.com>
2022-06-23 18:19:50 -0300

    ignt-50-00

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
+import { ApolloProvider } from "@apollo/client"
+import { client } from "./lib/apollo"
 import { Event } from "./pages/Event"
+import { Router } from "./Router"
+import { BrowserRouter } from "react-router-dom"
 
 function App() {
       return (
-        <Event />
+        <ApolloProvider client={client} >
+          <BrowserRouter>
+            <Router />
+          </BrowserRouter>
+        </ApolloProvider>
+        
   )
 }
 export default App
