62f93975e1887ba09b4f236f04929dbd3d294b9d
ttiago12 <74989635+ttiago12@users.noreply.github.com>
2022-06-23 15:26:36 -0300

    ignt-aula2

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -1,5 +1,6 @@
 import { CheckCircle , Lock } from 'phosphor-react'
-
+import { isPast , format } from 'date-fns'
+import ptBR from  'date-fns/locale/pt-BR'
 
 interface LessonProps{
   title: string;
@@ -9,11 +10,14 @@ interface LessonProps{
 }
 
 export function Lesson(props: LessonProps){
-  const isLessonAvailable = false;
+  const isLessonAvailable = isPast(props.availableAt);
+  const availableDateFormatted = format(props.availableAt, "EEEE ' • 'd' de 'MMMM' • 'k'h'mm",{
+    locale: ptBR,
+  })
   return(
     <a href="#">
       <span className="text-gray-300 ">
-        {props.availableAt.toString()}
+        {availableDateFormatted}
       </span>
       <div className="rounded border border-gray-500 p-4 mt-2">
         <header className="flex items-center justify-between">
