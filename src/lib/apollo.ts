06b7574f3185f1bee2d8bafda77fe36f76261cd7
ttiago12 <74989635+ttiago12@users.noreply.github.com>
2022-06-25 20:57:48 -0300

    aula4-33

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,6 +1,9 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-  uri: 'https://api-sa-east-1.graphcms.com/v2/cl4pm8xtt204p01z48664gqyw/master',
+  uri: import.meta.env.VITE_API_URL,
+  headers:{
+    'Autorization': `Bearer ${import.meta.env.VITE_API_ACCESS_TOKEN}`
+  },
   cache: new InMemoryCache(),
 })
