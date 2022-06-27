fc5f7fe29f3c347907bf54877a97770e53dce256
ttiago12 <74989635+ttiago12@users.noreply.github.com>
2022-06-27 13:35:53 -0300

    changes

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -2,6 +2,7 @@ import { CheckCircle , Lock } from 'phosphor-react'
 import { isPast , format } from 'date-fns'
 import ptBR from  'date-fns/locale/pt-BR'
 import { Link, useParams, useSearchParams } from 'react-router-dom';
+import classNames from 'classnames';
 
 interface LessonProps{
   title: string;
@@ -25,10 +26,17 @@ const isActiveLesson = slug == props.slug;
       <span className="text-gray-300 ">
         {availableDateFormatted}
       </span>
-      <div className={`rounded border border-gray-500 p-4 mt-2 group-hover:border-green-500 ${}`}>
+      
+      <div 
+        className={classNames('rounded border border-gray-500 p-4 mt-2 group-hover:border-green-500',{
+          'bg-green-500' :isActiveLesson, 
+        })}>
         <header className="flex items-center justify-between">
           {isLessonAvailable ? (
-          <span className="text-sm text-blue-500 font-medium flex items-center gap-2">
+          <span className={classNames('text-sm font-medium flex items-center gap-2', {
+            'text-white': isActiveLesson,
+            'text-blue-500': isActiveLesson,
+          })}>
               <CheckCircle size={20} />
               Conteúdo liberado
           </span>
@@ -39,11 +47,20 @@ const isActiveLesson = slug == props.slug;
           </span>
           )}
 
-          <span className="text-xs rounded py-[0.125rem] px-2 text-white border border-green-300 font-bold">
+
+
+          <span className={classNames('text-xs rounded py-[0.125rem] px-2 text-white border border-green-300 font-bold',{
+            'border-white': isActiveLesson,
+            'border-green-300': !isActiveLesson,
+          }
+            )}>
             {props.type == 'live' ? 'AO VIVO' : 'AULA PRÁTICA'}
           </span>
         </header>
-        <strong className="text-gray-200 mt-5 block">
+        <strong className={classNames('mt-5 block',{
+          'text-white': isActiveLesson,
+          'text-gray-200': !isActiveLesson, 
+        })}>
           {props.title}
         </strong>
       </div>
